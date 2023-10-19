a25b8975e4c4bacccfb92ee74f86f0d5bea7b073
Ozurmba chibuike <ocbmf17@gmail.com>
2023-10-19 23:40:54 +0100

    done
    done

--- controllers/RaffleControllers.js
+++ controllers/RaffleControllers.js
@@ -119,14 +119,17 @@ exports.GetSingleRaffle= async (req,res)=>{
 
 exports.UpdateSingleRaffle=async (req,res)=>{
     try {
-        
-
-        const{id}=req.params
-        const data=await DB.findByIdAndUpdate(id,req.body)
-        
-        res.status(200).json(data)
-
-
+        const { id } = req.params;
+        const updatedData = req.body;
+    
+        // Use the findByIdAndUpdate method to update the document by ID
+        const data = await DB.findByIdAndUpdate(id, updatedData, { new: true });
+    
+        if (!data) {
+            return res.status(404).json({ message: 'Document not found' });
+        }
+    
+        res.status(200).json(data,updatedData);
     } catch (error) {
         res.status(500).json({
             message:error.message
