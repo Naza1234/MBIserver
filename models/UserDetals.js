2dcb1e439d922cd581c74a2dc36139e36f7d2539
Ozurmba chibuike <ocbmf17@gmail.com>
2024-01-07 19:51:32 +0100

    done
    done

--- models/UserDetals.js
+++ models/UserDetals.js
@@ -10,6 +10,11 @@ const UserSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    UserIsAdmin: {
+        type:Boolean,
+        required:true,
+        default:false
+    },
     UserEmail: {
         type: String,
         required: true
